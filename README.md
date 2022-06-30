ac0cef91b40d64d8b8c6f97ec813419614a6570c
John Browder <johnbrowder@Johns-MacBook-Air.local>
2022-06-30 16:54:05 -0400

    Deliverable2-summary stats on Suspension Coils

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # MechaCar_Statistical_Analysis
+
+## Overview
+
+## Linear Regression to Predict MPG
+
+![This is an image](https://github.com/JDBrowder523/MechaCar_Statistical_Analysis/blob/main/Images/D1_Summary_Stats_Mecha_mpg.png)
+
+ - Summary statistics were generated using RStudio to see which factors provided a non-random variance on miles per gallon(mpg).  The two factors that provided statistically significant non-random variance are vehicle length and ground clearance.  These factors had p-values of 2.6e-12 and 5.21e-8, respectively, which is less than the threshold of 0.05. 
+
+ - The slop of the regression line is 5.35e-11.  This is considered to be 0 slope.
+
+ - This linear regression model does predict the mpg of the MechaCar effectively due to the r^2 value of .7149. Effectively, the linear regression model is 71.49% accurate.
+
--- R_Analysis_Challenge/MechaCarChallenge.R
+++ R_Analysis_Challenge/MechaCarChallenge.R
@@ -8,3 +8,14 @@ Mecha_mpg <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
 #Linear regression on all 6 variables
 lm( mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=Mecha_mpg) #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=Mecha_mpg))#generate summary statistics
+
+# DELIVERABLE 2 CODE
+
+# Read in csv file 'Suspension_Coil.csv'
+Suspension_coil <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors=F)
+
+# Create total_summary
+total_summary <- Suspension_coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+# Create grouped lot_summary using group_by
+lot_summary <- Suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
